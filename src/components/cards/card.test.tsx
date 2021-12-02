db16ac02d14d2d985ff75daec606a3a096bcca46
Richard <richard.forsberg@iths.se>
2021-12-02 09:33:50 +0100

    added products card component and tests

--- src/components/cards/card.test.tsx
+++ src/components/cards/card.test.tsx
@@ -1,17 +1,57 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import ProductCardComponent from "./product-card.component";
-import { shallow } from "enzyme";
+import { shallow, render as enzymeRender, mount } from "enzyme";
+import { IProduct } from "../../models/product";
+import { BrowserRouter } from "react-router-dom";
 
 describe("card.component.tsx", () => {
+  const props: Partial<IProduct> = {
+    name: "Spongebob",
+    img: "facebook/1",
+    price: 200,
+    product_id: "sdadoii",
+  };
   describe("Smoke Test", () => {
     it("should render card.component.tsx", () => {
-      render(<ProductCardComponent />);
+      render(
+        <BrowserRouter>
+          <ProductCardComponent />
+        </BrowserRouter>
+      );
     });
   });
-  describe("Black Box", () => {
-    it("should render card.component.tsx", () => {
-      render(<ProductCardComponent />);
+  describe("Black Box Tests", () => {
+    it("Should have a header section", () => {
+      const wrapper = shallow(<ProductCardComponent />);
+      const header = wrapper.find('[data-test="header-section"]');
+      expect(header.exists()).toBe(true);
+    });
+    it("Should have a content section", () => {
+      const wrapper = shallow(<ProductCardComponent />);
+      const content = wrapper.find('[data-test="content-section"]');
+      expect(content.exists()).toBe(true);
+    });
+    it("Should have a footer section", () => {
+      const wrapper = shallow(<ProductCardComponent />);
+      const footer = wrapper.find('[data-test="footer-section"]');
+      expect(footer.exists()).toBe(true);
+    });
+  });
+
+  describe("White Box Tests", () => {
+    it("Should have a link to a single product", () => {
+      const wrapper = shallow(<ProductCardComponent />);
+      const link = wrapper.find('[data-test="product-link"]');
+      expect(link.exists()).toBe(true);
+    });
+    it("Should have props", () => {
+      const wrapper = mount(
+        <BrowserRouter>
+          <ProductCardComponent name="spongebob" />
+        </BrowserRouter>
+      );
+      const product = wrapper.find(ProductCardComponent);
+      expect(product.props().name).toEqual("spongebob");
     });
   });
 });
---
+++ src/models/product.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  product_id: string;
+  img: string;
+  name: string;
+  price: number;
+  shortDescription: string;
+  description: string;
+}
