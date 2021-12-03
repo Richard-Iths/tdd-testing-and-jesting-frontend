859d5e2b1cb4b029fe7583329b93a6871a771742
richard-iths <richard.forsberg@iths.se>
2021-12-03 09:39:26 +0100

    finished tests for landing page

--- src/components/cards/card.test.tsx
+++ src/components/cards/card.test.tsx
@@ -1,38 +1,38 @@
 import { render } from "@testing-library/react";
-import ProductCardComponent from "./product-card.component";
+import ProductCardComponent,{Props} from "./product-card.component";
 import { shallow, render as enzymeRender, mount } from "enzyme";
-import { IProduct } from "../../models/product";
 import { BrowserRouter } from "react-router-dom";
 
 describe("card.component.tsx", () => {
-  const props: Partial<IProduct> = {
+  const props: Props = {
     name: "Spongebob",
     img: "facebook/1",
     price: 200,
     product_id: "sdadoii",
+    shortDescription:"a short description",
   };
   describe("Smoke Test", () => {
     it("should render card.component.tsx", () => {
       render(
         <BrowserRouter>
-          <ProductCardComponent />
+          <ProductCardComponent {...props}/>
         </BrowserRouter>
       );
     });
   });
   describe("Black Box Tests", () => {
     it("Should have a header section", () => {
-      const wrapper = shallow(<ProductCardComponent />);
+      const wrapper = shallow(<ProductCardComponent {...props}/>);
       const header = wrapper.find('[data-test="header-section"]');
       expect(header.exists()).toBe(true);
     });
     it("Should have a content section", () => {
-      const wrapper = shallow(<ProductCardComponent />);
+      const wrapper = shallow(<ProductCardComponent {...props} />);
       const content = wrapper.find('[data-test="content-section"]');
       expect(content.exists()).toBe(true);
     });
     it("Should have a footer section", () => {
-      const wrapper = shallow(<ProductCardComponent />);
+      const wrapper = shallow(<ProductCardComponent {...props}/>);
       const footer = wrapper.find('[data-test="footer-section"]');
       expect(footer.exists()).toBe(true);
     });
@@ -40,14 +40,14 @@ describe("card.component.tsx", () => {
 
   describe("White Box Tests", () => {
     it("Should have a link to a single product", () => {
-      const wrapper = shallow(<ProductCardComponent />);
+      const wrapper = shallow(<ProductCardComponent  {...props}/>);
       const link = wrapper.find('[data-test="product-link"]');
       expect(link.exists()).toBe(true);
     });
     it("Should have props", () => {
       const wrapper = mount(
         <BrowserRouter>
-          <ProductCardComponent name="spongebob" />
+          <ProductCardComponent {...props} name="spongebob" />
         </BrowserRouter>
       );
       const product = wrapper.find(ProductCardComponent);
--- src/pages/landing.page.test.tsx
+++ src/pages/landing.page.test.tsx
@@ -1,17 +1,33 @@
-import { render, } from "@testing-library/react";
-import { shallow, render as enzymeRender, mount, ReactWrapper } from "enzyme";
+import { render } from "@testing-library/react";
+import { mount, ReactWrapper } from "enzyme";
 import { BrowserRouter } from "react-router-dom";
 import ProductCardComponent from "../components/cards/product-card.component";
 import LandingPage from "./landing.page";
 import { IProduct } from "../models/product";
 import axios, { AxiosResponse } from "axios";
 import { act } from "react-dom/test-utils";
-import React from 'react';
+import React from "react";
 
-// jest.mock("axios");
-// const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+afterAll(() => {
+  jest.resetAllMocks();
+})
 describe("landing.page.tsx", () => {
+  let fakeData :IProduct[];
+  beforeEach(() => {
+    fakeData = [
+      {
+        description: "amazing product",
+        img: "sko.gif",
+        name: "Trasiga skor",
+        price: 150,
+        product_id: "1",
+        shortDescription: "skor",
+      },
+    ];
+  })
   describe("smoke tests", () => {
     it("should render landing page", () => {
       render(
@@ -21,16 +37,6 @@ describe("landing.page.tsx", () => {
       );
     });
     it("should render product-card component", async () => {
-      const fakeData: IProduct[] = [
-        {
-          description: "amazing product",
-          img: "sko.gif",
-          name: "Trasiga skor",
-          price: 150,
-          product_id: "1",
-          shortDescription: "skor",
-        },
-      ];
       const response: Partial<AxiosResponse> = {
         data: {
           data: fakeData,
@@ -38,38 +44,102 @@ describe("landing.page.tsx", () => {
       };
 
       await act(async () => {
-      axios.get = jest.fn().mockReturnValue(response);
-      })
+        // axios.get = jest.fn().mockReturnValue(response);
+        mockedAxios.get.mockReturnValue(Promise.resolve(response));
+      });
 
-      const effectSpy = jest.spyOn(React,"useEffect");
 
-  
-      let wrapper : ReactWrapper;
+      let wrapper: ReactWrapper;
+
       await act(async () => {
-         wrapper = mount(
+        wrapper = mount(
           <BrowserRouter>
-          <LandingPage />
-        </BrowserRouter>
-      );
-        expect(effectSpy).not.toHaveBeenCalled();
-        expect(axios.get).not.toHaveBeenCalled();
-      })
-      
-      
-      await act(async () => {   
+            <LandingPage />
+          </BrowserRouter>
+        );
+      });
+
+      await act(async () => {
         wrapper.update();
-        expect(axios.get).toHaveBeenCalledTimes(1);
-        expect(effectSpy).toHaveBeenCalledTimes(1);
-        const product = wrapper.find(
-          ProductCardComponent
-        ).first();
+        const product = wrapper.find(ProductCardComponent).first();
         expect(product.exists()).toBe(true);
-        expect(product.props()).toStrictEqual(fakeData[0]);
       });
     });
   });
 
-  describe("black box tests", () => {});
+  describe("black box tests", () => {
+    it("should have called the rest api products endpoint via useEffect hook", async () => {
+      const response: Partial<AxiosResponse> = {
+        data: {
+          data: fakeData,
+        },
+      };
+
+      await act(async () => {
+        mockedAxios.get.mockReturnValue(Promise.resolve(response));
+      });
+
+      const effectSpy = jest.spyOn(React, "useEffect");
+
+      let wrapper: ReactWrapper;
+
+      await act(async () => {
+        wrapper = mount(
+          <BrowserRouter>
+            <LandingPage />
+          </BrowserRouter>
+        );
+        expect(effectSpy).not.toHaveBeenCalled();
+        expect(mockedAxios.get).not.toHaveBeenCalled();
+      });
+
+      await act(async () => {
+        wrapper.update();
+        expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+        expect(effectSpy).toHaveBeenCalledTimes(1);
+      });
+     
+    })
+    it("should contain rendered product card component, with props from rest api", async () => {
+      fakeData.push({
+        description: "amazing product2",
+        img: "sko2.gif",
+        name: "Trasiga skor2",
+        price: 1502,
+        product_id: "2",
+        shortDescription: "skor2",
+      })
+      const response: Partial<AxiosResponse> = {
+        data: {
+          data: fakeData,
+        },
+      };
+
+      await act(async () => {
+        mockedAxios.get.mockReturnValue(Promise.resolve(response));
+      });
+
+
+      let wrapper: ReactWrapper;
+
+      await act(async () => {
+        wrapper = mount(
+          <BrowserRouter>
+            <LandingPage />
+          </BrowserRouter>
+        );
+      });
+
+      await act(async () => {
+        wrapper.update();
+        const products = wrapper.find(ProductCardComponent);
+        const [firstProduct,secondProduct] = fakeData;
+        expect(products.length).toBe(2);
+        expect(products.first().props()).toStrictEqual(firstProduct);
+        expect(products.at(1).props()).toStrictEqual(secondProduct);
+      });
+    })
+  });
 
   describe("white box tests", () => {});
 });
